46d026ae23f3453e067d1465e7d960a5706f7398
Natalia <PalejNatalia@gmail.com>
2024-11-08 18:54:58 +0000

    Added placeholders for input fields

--- NPalej_A00279259/src/makeup_store/Register.java
+++ NPalej_A00279259/src/makeup_store/Register.java
@@ -1,8 +1,11 @@
 package makeup_store;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -26,42 +29,50 @@ public class Register extends JFrame {
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
-        // Panel with 10 rows 2 columns
-        panel.setLayout(new GridLayout(10, 2));
+        // Panel with 11 rows 2 columns
+        panel.setLayout(new GridLayout(11, 2));
         // Add margin to panel (top, left, bottom, right)
         panel.setBorder(new EmptyBorder(10, 20, 10, 20));
-
+        
         // Customer Details Fields
         panel.add(new JLabel("First Name*:"));
         firstNameField = new JTextField();
+        setPlaceholder(firstNameField, "Your Name");
         panel.add(firstNameField);
 
         panel.add(new JLabel("Last Name*:"));
         lastNameField = new JTextField();
+        setPlaceholder(lastNameField, "Your Last Name");
         panel.add(lastNameField);
 
         panel.add(new JLabel("Address Line 1*:"));
         address1Field = new JTextField();
+        setPlaceholder(address1Field, "Your Address");
         panel.add(address1Field);
 
         panel.add(new JLabel("Address Line 2:"));
         address2Field = new JTextField();
+        setPlaceholder(address2Field, "Your Address (Optional)");
         panel.add(address2Field);
 
         panel.add(new JLabel("City*:"));
         cityField = new JTextField();
+        setPlaceholder(cityField, "City");
         panel.add(cityField);
 
         panel.add(new JLabel("Eircode:"));
         eircodeField = new JTextField();
+        setPlaceholder(eircodeField, "A01 B2C34");
         panel.add(eircodeField);
 
         panel.add(new JLabel("Phone Number*:"));
         phoneNoField = new JTextField();
+        setPlaceholder(phoneNoField, "089 123 4567");
         panel.add(phoneNoField);
 
         panel.add(new JLabel("Email Address*:"));
         emailField = new JTextField();
+        setPlaceholder(emailField, "youremail@example.com");
         panel.add(emailField);
         
         // Mandatory fields info
@@ -75,7 +86,6 @@ public class Register extends JFrame {
         registerButton.addActionListener(new registerAction());
         panel.add(registerButton);
         panel.add(new JLabel());
-
 
         add(panel);
         setVisible(true);
@@ -126,5 +136,31 @@ public class Register extends JFrame {
             statement.setString(8, email);
             statement.executeUpdate();
         }
+    }
+    
+    // Method to add placeholder for insert fields
+    private void setPlaceholder(JTextField textField, String placeholder) {
+        // Set initial placeholder text
+        textField.setText(placeholder);
+        textField.setForeground(Color.GRAY);
+
+        // Add focus listener to placeholder
+        textField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textField.getText().equals(placeholder)) {
+                    textField.setText("");
+                    textField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (textField.getText().isEmpty()) {
+                    textField.setText(placeholder);
+                    textField.setForeground(Color.GRAY);
+                }
+            }
+        });
     }
 }
