6371e05379a46fe455a94e497a211da8ed9fdc6a
Natalia <PalejNatalia@gmail.com>
2024-11-10 01:48:49 +0000

    Started implementing ViewOrdersContent page for customer

---
+++ NPalej_A00279259/src/makeup_store/ViewOrdersContent.java
@@ -0,0 +1,14 @@
+package makeup_store;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JInternalFrame;
+
+public class ViewOrdersContent extends JInternalFrame implements ActionListener {
+	
+	public ViewOrdersContent(int customer_id) throws NataliaException {
+		super("View My Orders", false, false, false, false);
+		
+	}
+
+}
